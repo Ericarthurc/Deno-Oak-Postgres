a2a568dde4efe83479e1bf88073f6dd7038b5aff
Eric Christensen <hebrew.8.12@gmail.com>
2021-10-10 12:26:05 -0700

    implicit imports

--- deps.ts
+++ deps.ts
@@ -1,5 +1,9 @@
-export { config as envConfig } from "https://deno.land/x/dotenv/mod.ts";
-import * as colors from "https://deno.land/std/fmt/colors.ts";
+export { config as envConfig } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
+import * as colors from "https://deno.land/std@0.110.0/fmt/colors.ts";
 export { colors };
-export { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
-export { Client as PostgresClient } from "https://deno.land/x/postgres/mod.ts";
+export {
+  Application,
+  Context,
+  Router,
+} from "https://deno.land/x/oak@v9.0.1/mod.ts";
+export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.13.0/mod.ts";
